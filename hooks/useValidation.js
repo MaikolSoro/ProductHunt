2d770db3145dc6f5500c529303384caf7bb6f409
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-29 15:22:35 -0600

    A hook was created for form validations

---
+++ hooks/useValidation.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+const useValidation = (stateInicial, validar, funcion) => {
+
+	const[valores, guardarValores] = useState(stateInicial);
+	const[errores, guardarErrores] = useState({});
+	const[submitForm, guardarSubmitForm ] = useState(false);
+	
+	useEffect(() => {
+		if(submitForm) {
+			const noErrores = Object.keys(errores).length === 0;
+
+			if(noErrores) {
+				funcion(); // Función que se ejecuta en el componente
+			}
+			guardarSubmitForm(false); 
+		}
+	}, []);
+
+	//Función  que se ejecuta conforme el usuario escribe algo
+	const handleChange = e => {
+		guardarValores({
+			...valores,
+			[e.target.name] : e.target.value
+		})
+	}
+
+	// Función que se ejecuta cuando el usuario hace submit
+	const handleSubmit = e => {
+		e.preventDefault();
+		const erroresValidaciones = validar(valores);
+		guardarErrores(erroresValidaciones);
+		guardarSubmitForm(true);
+	}
+
+	return {
+		valores,
+		errores,
+		submitForm,
+		handleSubmit,
+		handleChange
+
+	};
+}
+ 
+export default useValidation;
