735d4b369e9954ed7e1bbd40f4b66dd3296c94d5
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-28 21:50:14 -0600

    CSS to header

--- componets/layouts/Header.js
+++ componets/layouts/Header.js
@@ -2,13 +2,45 @@ import React from 'react';
 import Buscar from '../ui/Buscar';
 import Navegation from './Navegation';
 import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const ContenedorHeader = styled.div`
+	max-width: 1200px;
+	width: 95%9;
+	margin:0 auto;
+	@media (min-width: 768px) {
+		display: flex;
+		justify-content: space-between;
+	}
+`;
+const Logo = styled.a`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
 
 const Header = () => {
 	return ( 
-			<header>
-				<div>
-					<div>
-						<p>P</p>
+			<header
+			 css={css`
+				 border-bottom: 2px solid var(--gris3);
+				 padding: 1rem 0;
+				 
+			 `}>
+				<ContenedorHeader>
+					<div  css={css`
+                        display:flex;
+                        align-items: center;
+                    `}
+					>
+						<Link href="/">
+							<Logo>P</Logo>
+
+						</Link>
 							<Buscar />
 
 							<Navegation />
@@ -22,7 +54,7 @@ const Header = () => {
 						<Link href= "/">Login</Link>
 						<Link href= "/">Crear Cuenta</Link>
 					</div>
-				</div>
+				</ContenedorHeader>
 			</header>
 	 );
 }
