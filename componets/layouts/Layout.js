6f5275ab46744fad256e250fe7f7860142d6b5d8
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-28 21:37:19 -0600

    Adding images and css, google fonts fonts

--- componets/layouts/Layout.js
+++ componets/layouts/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
 import Header from './Header';
-import { Gobal, css} from  '@emotion/core';
+import { Global, css} from  '@emotion/core';
+import Head from 'next/head';
 
-const Layout = props => {
+const Layout = (props) => {
 	return (  
 		<>
 			 <Global 
@@ -49,6 +50,13 @@ const Layout = props => {
                         max-width: 100%;
                     }
                 `} />
+
+				<Head>
+					<title>Product Hunt Firebase y Next.js</title>
+					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossOrigin="anonymous" />
+					<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto+Slab:400,700&display=swap" rel="stylesheet" />
+					<link href="/static/css/app.css" rel="stylesheet" />
+            </Head>
 			<Header/>
 			<main>{props.children}</main>
 		</>
