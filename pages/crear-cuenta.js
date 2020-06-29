1b672d0c0d34fffd706e86490f58e1e2e5ca109a
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-29 14:32:42 -0600

    Creating the form to create accounts

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -7,7 +7,43 @@ const CrearCuenta = () => (
   
 	<div>
 		<Layout>
+			<>
 			<h1>Crear Cuenta</h1>
+			<form>
+				<div>
+					<label htmlFor="nombre">Nombre</label>
+						<input type="Text"
+						id="nombre"
+						placeholder="Tu nombre"
+						name= "nombre"
+					 />
+				</div>
+
+				<div>
+					<label htmlFor="email">Email</label>
+						<input type="email"
+						id="email"
+						placeholder=" Tu Email"
+						name= "email"
+					 />
+				</div>
+
+				<div>
+					<label htmlFor="password">Password</label>
+						<input type="password"
+						id="password"
+						placeholder=" Tu Password"
+						name= "password"
+					 />
+				</div>
+
+				<input 
+						type ="submit"
+						value="Crear cuenta" 
+					/>
+
+			</form>
+			</>
 		</Layout>
 	</div>
 )
