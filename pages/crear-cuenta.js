458bae2afedc0edb1a190ac52939b45a0dd84fd8
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-29 14:53:40 -0600

    CSS for forms

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { css } from '@emotion/core';
 import Layout  from '../componets/layouts/Layout';
-
+import { Formulario, Campo, InputSubmit } from '../componets/ui/Formulario';
 
 
 const CrearCuenta = () => (
@@ -8,41 +9,47 @@ const CrearCuenta = () => (
 	<div>
 		<Layout>
 			<>
-			<h1>Crear Cuenta</h1>
-			<form>
-				<div>
+			<h1
+			css={css`
+			text-align: center;
+			margin-top: 5rem;
+			`}
+			>Crear Cuenta</h1>
+			<Formulario>
+				<Campo>
 					<label htmlFor="nombre">Nombre</label>
 						<input type="Text"
 						id="nombre"
 						placeholder="Tu nombre"
 						name= "nombre"
 					 />
-				</div>
+				</Campo>
 
-				<div>
+				<Campo>
 					<label htmlFor="email">Email</label>
 						<input type="email"
 						id="email"
 						placeholder=" Tu Email"
 						name= "email"
 					 />
-				</div>
+				</Campo>
 
-				<div>
+				<Campo>
 					<label htmlFor="password">Password</label>
 						<input type="password"
 						id="password"
 						placeholder=" Tu Password"
 						name= "password"
 					 />
-				</div>
+				</Campo>
 
-				<input 
+				<InputSubmit 
 						type ="submit"
 						value="Crear cuenta" 
 					/>
+					
 
-			</form>
+			</Formulario>
 			</>
 		</Layout>
 	</div>
