c75ec15793aa916b13680762c81a212dd983734c
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-28 20:34:23 -0600

    Routing in Next.js

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Layout  from '../componets/layouts/Layout';
 
-
-
-export default function Home() {
+export default function Nosotros() {
   return (
     <div>
         <Layout>
-          <h1>Inicio</h1>
+          <h1>Nosotros</h1>
         </Layout>
     </div>
   )
