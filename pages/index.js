f5fa331bca38dfa6c3a541b64f57466425891d4a
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-30 15:26:31 -0600

    consult the database

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState, useContext} from 'react';
 import Layout  from '../componets/layouts/Layout';
+import { FirebaseContext } from '../firebase';
 
+const [productos, guardarProductos] = useState([]);
+const { firebase } = useContext(FirebaseContext);
 
+useEffect(() =>{
+  const obtenerProductos = ()=> {
+    firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot)
+  }
+  obtenerProductos();
+
+}, []);
+
+function manejarSnapshot(snapshot) {
+  const productos = snapshot.docs.map(doc => {
+    return {
+      id:doc.id,
+      ...doc.data()
+    }
+  });
+  guardarProductos(productos);
+}
 
 export default function Home() {
   return (
