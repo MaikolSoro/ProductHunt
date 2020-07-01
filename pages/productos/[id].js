8d6371437bbd255a4365168dca72c23ca0650161
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-01 13:20:55 -0600

    validate that a product exists

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { FirebaseContext } from '../../firebase';
+import Layout from '../../componets/layouts/Layout';
+
+import Error404 from '../../componets/layouts/404';
+
 
 const Producto = () => {
-	return ( <h1>Desde [id].js	</h1> );
-}
- 
+
+	// state del componente
+	const [producto, guardarProducto] = useState({});
+	const [error, guadarError] = useState(false);
+	
+
+
+	//Routing para obtener el id actual
+	const router = useRouter();
+	const { query: { id }} = router;
+
+	// context de firebase
+	const { firebase } = useContext(FirebaseContext);
+
+	useEffect(() =>{
+			if(id) {
+				console.log('Ya hay in id', id);
+			}
+			obtenerProducto = async () => {
+				const productQuery = await firebase.db.collection('productos').doc(id);
+				const producto = await productoQuery.get();
+				
+				if(producto.exists()) {
+					guardarProducto( producto.data());
+					console.log('si existe')
+				} else {
+					 guadarError( true );
+					console.log('no existe')
+				}
+			}
+
+			obtenerProducto();
+	},[id]);
+
+	if(error) return <Error404 />
+	return (
+			<Layout>
+				<>
+				{ error &&  <Error404 /> }
+				</>
+				
+			</Layout>
+		);	
+	
+}	
+	
 export default Producto;
