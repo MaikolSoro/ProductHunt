721ede3493ca0195e0dc60a7f81dca7e33a7fb4f
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-02 13:18:23 -0600

    Displaying Products by creator and by votes

--- pages/populares.js
+++ pages/populares.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Layout  from '../componets/layouts/Layout';
+import DetallesProducto from '../componets/layouts/DetalleProducto';
+import useProductos from '../hooks/useProducto';
 
-
-
-const Populares = () => (
+const Populares = ()=> {
   
-	<div>
-		<Layout>
-			<h1>Populares</h1>
-		</Layout>
-	</div>
-)
-export default Populares
-
+	const { productos } = useProductos('votos');
+	return (
+		<div>
+		  <Layout>
+			<div className="listado-productos">
+				<div className="contenedor">
+				  <ul className="bg-white">
+					  {productos.map(producto => (
+						  <DetallesProducto
+							  key={producto.id}
+							  producto={producto}
+						  />
+					  ))}
+				  </ul>
+				</div>
+			</div>
+		  </Layout>
+		</div>
+	  )
+	}
+	
+	export default Populares
